5a8834f4dabbf77596075870f4891c0a1b081d7f
Abban04 <92086872+abban04@users.noreply.github.com>
2023-05-20 19:08:40 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const link = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(link).then((response) => {
+        setData(response.data)
+      })
+      setLocation('')
+    }
+  }
+
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Välj plats'
+          type="text" />
+      </div>
+
+      <div className="container">
+          <div className="location">
+            <h2>{data.name}</h2>
+          </div>
+
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+
+          <div className="description">
+            {data.weather ? <h2>Väder: {data.weather[0].main}</h2> : null}
+            <br></br>
+            <div className="wind">
+              {data.wind ? <p className='bold'>Vindhastighet: {data.wind.speed.toFixed()} MPH</p> : null}
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
